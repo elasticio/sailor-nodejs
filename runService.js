9f2c93b06d4539af63d9ff1996c081ce9fdf451f
ellenaua <elenasharovar@gmail.com>
2015-11-18 18:20:54 +0200

    Changed env vars retrieval logics in service.js

--- runService.js
+++ runService.js
@@ -1,10 +1,10 @@
-var Q = require('q');
+var env = require('./lib/settings').init(process.env);
 var logging = require('./lib/logging');
 var service = require('./lib/service');
 
 var serviceMethod = process.argv[2];
 
-service.processService(serviceMethod, process.env)
+service.processService(serviceMethod)
     .catch(logging.criticalError)
     .done();
 
