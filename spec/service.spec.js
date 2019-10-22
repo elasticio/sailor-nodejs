ffbbadfe3323d78b0e817657d4be053f6d693cce
Hannah Bulmer <hannah.bulmer@elastic.io>
2019-10-22 17:46:32 +0200

    remove comma

--- spec/service.spec.js
+++ spec/service.spec.js
@@ -253,7 +253,7 @@ describe('Service', () => {
                     function checkResult(result) {
                         expect(result.status).toEqual('success');
                         expect(result.data).toEqual({
-                            verified: true,
+                            verified: true
                         });
                     }
                 });
