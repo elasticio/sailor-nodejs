ccc31a3c459ff4a81a5266faeeba40582bab4029
Mykhailo Bortnyk <legendmiha115@gmail.com>
2025-12-17 20:05:31 +0000

    Proxy reconnection

--- spec/component/triggers/data_trigger.js
+++ spec/component/triggers/data_trigger.js
@@ -2,7 +2,7 @@ exports.process = processTrigger;
 
 async function processTrigger(msg, cfg) {
     var that = this;
-    //that.emit('data', { body: { items: [1,2,3,4,5,6] } });
-    await that.emit('data', { body: 'a'.repeat(1048576), headers: { testHeader: 'headerValue' } });
+    await that.emit('data', { body: { items: [1,2,3,4,5,6] } });
+    //await that.emit('data', { body: 'a'.repeat(1048576), headers: { testHeader: 'headerValue' } });
     await that.emit('end');
 }
