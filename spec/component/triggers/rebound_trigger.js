838a3858a5efa84a4fd72d1f9d649a587177a46b
Michael Bortnyk <legendmiha115@gmail.com>
2026-04-03 20:38:27 +0300

    Change auth to JWT

--- spec/component/triggers/rebound_trigger.js
+++ spec/component/triggers/rebound_trigger.js
@@ -2,6 +2,6 @@ exports.process = processTrigger;
 
 async function processTrigger(msg, cfg) {
     var that = this;
-    await that.emit('error', new Error('Rebound reason'));
+    await that.emit('rebound', new Error('Rebound reason'));
     await that.emit('end');
 }
