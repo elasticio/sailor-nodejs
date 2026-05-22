9be2a85db98f2ea46003375dcc4c34ea5f4b7db1
Michael Bortnyk <legendmiha115@gmail.com>
2026-05-22 16:22:12 +0300

    More fixes for incident recovey

--- spec/component/triggers/data_trigger.js
+++ spec/component/triggers/data_trigger.js
@@ -2,7 +2,7 @@ exports.process = processTrigger;
 
 async function processTrigger(msg, cfg) {
     var that = this;
-    await new Promise(resolve => setTimeout(resolve, 10000));
+    await new Promise(resolve => setTimeout(resolve, 120000));
     await that.emit('data', { body: { items: [1, 2, 3, 4, 5, 6] } });
     // await that.emit('data', { body: 'a'.repeat(1048576), headers: { testHeader: 'headerValue' } });
     await that.emit('end');
