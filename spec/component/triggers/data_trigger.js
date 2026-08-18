e1dc4c795c00465c8cc23d2df024f3d447cff9dd
Michael Bortnyk <legendmiha115@gmail.com>
2026-08-18 11:27:40 +0300

    Revert direct publish to maester instead of proxy

--- spec/component/triggers/data_trigger.js
+++ spec/component/triggers/data_trigger.js
@@ -4,6 +4,6 @@ async function processTrigger(msg, cfg) {
     var that = this;
     // await new Promise(resolve => setTimeout(resolve, 10000));
     // await that.emit('data', { body: { items: [1, 2, 3, 4, 5, 6] } });
-    await that.emit('data', { body: 'a'.repeat(10485761), headers: { testHeader: 'headerValue' } });
+    await that.emit('data', { body: 'a'.repeat(10 * 1024 * 1024), headers: { testHeader: 'headerValue' } });
     await that.emit('end');
 }
